e3e051132b9c8709e519a2455e6b7b9a0cc1d3dd
DueroBone <90635383+DueroBone@users.noreply.github.com>
2025-11-09 01:27:51 -0500

    Added example home page and explanation

--- lib/Views/Manual/home_page.dart
+++ lib/Views/Manual/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:tech_120_app/Views/Generated/home.dart';
+import 'package:tech_120_app/Views/Generated/home_page.dart';
 
+// Basically just renames Iphone161 to HomePage for clarity
+// This process is called wrapping
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
